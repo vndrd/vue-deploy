87aaa612ae53f915d6b8cc89c63e8b6a40476cc8
vndrd <ivangddzzz@gmail.com>
2020-05-13 19:30:41 -0400

    cleaning vue.config

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+/*module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? '/my-project/'
+      : '/'
+  }
+  */
+
+/*
+ module.exports = {
+    publicPath: '/v-prueba-deploy3'
+  }
+  */
